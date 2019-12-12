2a8ab841737279e8a96c29ccb93f6761accc3197
Julius Schumacher <juliusschumacher@gmail.com>
2019-12-12 18:22:59 +0100

    nu kan man automatisera sitt animekollande

--- animerad.sh
+++ animerad.sh
@@ -1,4 +1,37 @@
 #!/bin/bash
-SHOWS=$(trackma ls | grep results | head -c1)
+function shows() {
+    NUM=$(trackma ls | grep results | head -c1)
+    echo $NUM
+}
 
-echo $SHOWS
+function random_show() {
+    shuf -i 1-$(shows) -n 1
+
+}
+
+function play_show() {
+    SHOW=$(random_show)
+    echo -e "rescan \n play $SHOW \n update $SHOW" | trackma
+}
+
+function is_playing() {
+    top -bn1 | grep -q mpv
+    echo $?
+}
+
+function sleep_while_playing() {
+    while [ $(is_playing) -eq 0 ]
+    do
+        sleep 1
+    done
+}
+
+function main() {
+    while :
+    do
+        play_show
+        sleep_while_playing
+    done
+}
+
+main
